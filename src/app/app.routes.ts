4b8c5e6d7a7975d969fc7d3460546b462d6610d3
Nicolas Voges <nicolas.voges@hotmail.com>
2025-04-11 14:17:32 +0200

    avatar add transitions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AvatarComponent } from './components/landing-page/avatar/avatar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AvatarComponent}
+];
