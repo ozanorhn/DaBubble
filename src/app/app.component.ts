e6ea0a6176b0b5ac1e7bc2624698ee5897a6b1c7
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-04-11 19:44:34 +0200

    add user components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from "./components/shared/popUp/profile/profile.component";
 import { LogOutComponent } from "./components/shared/popUp/log-out/log-out.component";
 import { MembersComponent } from "./components/shared/popUp/members/members.component";
+import { AddUserComponent } from "./components/shared/popUp/add-user/add-user.component";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,8 @@ import { MembersComponent } from "./components/shared/popUp/members/members.comp
     RouterOutlet,
     ProfileComponent,
     LogOutComponent,
-    MembersComponent
+    MembersComponent,
+    AddUserComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
