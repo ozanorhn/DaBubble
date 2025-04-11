b3364c7b45bd833446eac809c4f751ff3172fd67
Ozan Orhan <ozanorhan60@icloud.com>
2025-04-11 13:12:01 +0200

    implement login + register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/landing-page/login/login.component";
+import { RegisterComponent } from "./components/landing-page/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
