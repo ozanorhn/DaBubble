7177a52b5c1093cebcf17955f276a84612842a4e
Nicolas Voges <nicolas.voges@hotmail.com>
2025-04-11 14:18:44 +0200

    remove route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
-import { AvatarComponent } from './components/landing-page/avatar/avatar.component';
 
-export const routes: Routes = [
-    {path: '', component: AvatarComponent}
-];
+export const routes: Routes = [];
