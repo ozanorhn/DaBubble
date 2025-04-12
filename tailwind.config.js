cb2fd257158f780722ff166f8a032fe597b57da8
Jean Pondy <manbayesse@gmail.com>
2025-04-12 10:18:30 +0200

    header und channelsPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        'primary': '#ECEEFE',
-        'purple1': '#444DF2',
-        'purple2': '#797EF3',
-        'purple3': '#535AF1',
-        'error': '#797EF3',
-        'online': '#92c83e',
-        'textgray': '#686868',
-        'lightpurple': '#adb0d9'
+      colors: {
+        header: "rgba(236, 238, 254, 0.8)",
+        primary: "#ECEEFE",
+        purple1: "#444DF2",
+        purple2: "#797EF3",
+        purple3: "#535AF1",
+        error: "#797EF3",
+        online: "#92c83e",
+        textgray: "#686868",
+        lightpurple: "#adb0d9",
+        btnBg: "#eff1fe",
       },
     },
   },
   plugins: [],
-}
-
+};
