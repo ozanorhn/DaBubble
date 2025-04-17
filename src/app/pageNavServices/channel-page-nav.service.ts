4567a747365529a65297e83b9609d4b54590ad4f
Nicolas Voges <nicolas.voges@hotmail.com>
2025-04-17 19:43:08 +0200

    main - mobile medium and big screens

--- src/app/pageNavServices/channel-page-nav.service.ts
+++ src/app/pageNavServices/channel-page-nav.service.ts
@@ -1,40 +1,69 @@
 import { Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { ChannelsService } from '../services/channels/channels.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChannelPageNavService {
   mobile = false;
-  showChannel = false;
-  showThread = false;
+  mediumScreen = false;
+  bigScreen = false;
+  channel = false;
+  thread = false;
   showDirectMessage = false;
+  
+  constructor(private router: Router, channelsService: ChannelsService) { }
+
+  showNav() {
+    return !this.channel && this.mobile && !this.thread || this.mediumScreen && !this.thread || this.bigScreen;
+  } 
+
+  showChannel() {
+    return this.channel && this.mobile && !this.thread || this.mediumScreen || this.bigScreen;
+  }
+
+  showThread() {
+    return !this.channel && this.mobile && this.thread || this.mediumScreen && this.thread || this.bigScreen;
+  }
+
+  checkScreenView() {
+    if (window.innerWidth >= 1024) {
+      if (!this.bigScreen) console.log('bigScreen');
+      this.bigScreen = true;
+      this.mediumScreen = false;
+      this.mobile = false;
+    } else if(window.innerWidth >= 768) {
+      if (!this.mediumScreen) console.log('mediumScreen');
+      this.bigScreen = false;
+      this.mediumScreen = true;
+      this.mobile = false;
+    } else {
+      if (!this.mobile) console.log('mobile');
+      this.bigScreen = false;
+      this.mediumScreen = false;
+      this.mobile = true;
+    }
+  }
 
   hideAddChannelPopUp = signal(true);
 
   hideAddUserPopUp = signal(true);
 
-  constructor(private router: Router) { }
-
-  navigate() {
-    if (window.innerWidth >= 1024) {
-      this.router.navigate(['/desktop-page'])
-    } else {
-      this.router.navigate(['/chat'])
-    }
-  }
-
   openChannel() {
-    if (this.mobile) {
-      this.showChannel = true;
-      this.showThread = false;
+    if (this.mobile || this.mediumScreen) {
+      this.channel = true;
+      this.thread = false;
     }
   }
 
   openThread() {
     if (this.mobile) {
-      this.showChannel = false;
-      this.showThread = true;
+      this.channel = false;
+      this.thread = true;
+    } else if (this.mediumScreen) {
+      this.channel = true;
+      this.thread = true;
     }
   }
 
