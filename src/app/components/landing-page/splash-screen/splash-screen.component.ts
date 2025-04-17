fc823b6da511e9d5b4716972e350af27e0f9109a
Ozan Orhan <ozanorhan60@icloud.com>
2025-04-17 11:28:18 +0200

    resolve splash error

--- src/app/components/landing-page/splash-screen/splash-screen.component.ts
+++ src/app/components/landing-page/splash-screen/splash-screen.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { trigger, style, animate, transition,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-splash-screen',
@@ -8,27 +7,6 @@ import { trigger, style, animate, transition,query,stagger } from '@angular/anim
   imports: [CommonModule],
   templateUrl: './splash-screen.component.html',
   styleUrl: './splash-screen.component.scss',
-  animations: [
-    trigger('logoAnimation', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translate(0, 0)' }), 
-        animate('0.5s ease-out', style({ opacity: 1, transform: 'translate(0, 0)' })) 
-      ]),
-      transition(':leave', [
-        animate('1s ease-in', style({ opacity: 0, transform: 'translate(-20%, -20%)' })) 
-      ])
-    ]),
-    trigger('letterAnimation', [
-      transition(':enter', [
-        query('span', [
-          style({ opacity: 0 }),
-          stagger('150ms', [  
-            animate('500ms', style({ opacity: 1 }))  
-          ])
-        ])
-      ])
-    ])
-  ]
 })
 
 export class SplashScreenComponent {
