c822504af95a6eedd0ac796d9e4547818cf5d0e5
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-04-19 00:38:12 +0200

    edit channel overlay responsive

--- src/app/components/shared/popUp/edit-channel/edit-channel.component.ts
+++ src/app/components/shared/popUp/edit-channel/edit-channel.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { MembersComponent } from "../members/members.component";
+import { EditDescriptionComponent } from "./edit-description/edit-description.component";
+import { EditNameComponent } from "./edit-name/edit-name.component";
 
 @Component({
   selector: 'app-edit-channel',
-  imports: [MembersComponent],
+  imports: [MembersComponent, EditDescriptionComponent, EditNameComponent],
   templateUrl: './edit-channel.component.html',
   styleUrl: './edit-channel.component.scss'
 })
---
+++ src/app/components/shared/popUp/edit-channel/edit-name/edit-name.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-name',
+  imports: [],
+  templateUrl: './edit-name.component.html',
+  styleUrl: './edit-name.component.scss'
+})
+export class EditNameComponent {
+
+}
