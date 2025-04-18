fdd42b1bb9099dbff7e21b6edcdffcbdc9b80834
Nicolas Voges <nicolas.voges@hotmail.com>
2025-04-18 19:34:45 +0200

    work on navigation service

--- src/app/pageNavServices/channel-page-nav.service.ts
+++ src/app/pageNavServices/channel-page-nav.service.ts
@@ -9,7 +9,7 @@ export class ChannelPageNavService {
   mobile = false;
   mediumScreen = false;
   bigScreen = false;
-  channel = false;
+  channel = true;
   thread = false;
   nav = true;
   showDirectMessage = false;
@@ -19,45 +19,74 @@ export class ChannelPageNavService {
 
   navigate() { }
 
-  showNav() {
-    return !this.channel && this.mobile && !this.thread && this.nav || this.mediumScreen && !this.thread && this.nav || this.bigScreen && this.nav;
-  }
-
   toggleNav() {
     if (this.nav) {
+      if (!this.channel && !this.thread || this.mediumScreen && !this.channel) {
+        this.channel = true;
+      }
       this.nav = false;
     } else {
       this.nav = true;
+      if (this.mediumScreen && this.channel && this.thread) {
+        this.channel = false;
+      }
+      if (this.mobile) {
+        this.channel = false;
+        this.thread = false;
+      }
     }
   }
 
+  showNav() {
+    return !this.channel && this.mobile && !this.thread && this.nav || this.mediumScreen && this.nav || this.bigScreen && this.nav;
+  }
+
   showChannel() {
-    return this.channel && this.mobile && !this.thread || this.mediumScreen || this.bigScreen;
+    return this.channel && this.mobile && !this.thread && !this.nav || this.mediumScreen && this.channel || this.bigScreen;
   }
 
   showThread() {
-    return !this.channel && this.mobile && this.thread || this.mediumScreen && this.thread || this.bigScreen && this.thread;
+    return !this.channel && this.mobile && this.thread && !this.nav || this.mediumScreen && this.thread || this.bigScreen && this.thread;
   }
 
   checkScreenView() {
     if (window.innerWidth >= 1280) {
-      if (!this.bigScreen) console.log('bigScreen');
-      this.bigScreen = true;
-      this.mediumScreen = false;
-      this.mobile = false;
+      this.setScreen('big');
     } else if (window.innerWidth >= 810) {
-      if (!this.mediumScreen) console.log('mediumScreen');
-      this.bigScreen = false;
-      this.mediumScreen = true;
-      this.mobile = false;
+      this.setScreen('medium');
+      this.adjustMediumScreen();
     } else {
-      if (!this.mobile) console.log('mobile');
-      this.bigScreen = false;
-      this.mediumScreen = false;
-      this.mobile = true;
-      if (this.channel && this.thread) {
-        this.channel = false;
-      }
+      this.setScreen('mobile');
+      this.adjustMobileScreen();
+    }
+  }
+
+  setScreen(size: 'big' | 'medium' | 'mobile') {
+    this.bigScreen = false;
+    this.mediumScreen = false;
+    this.mobile = false;
+    if (size === 'big') this.bigScreen = true;
+    if (size === 'medium') this.mediumScreen = true;
+    if (size === 'mobile') this.mobile = true;
+  }
+
+  adjustMediumScreen() {
+    if (this.thread && this.nav) {
+      this.channel = false;
+    } else if (!this.nav && !this.channel && this.thread) {
+      this.channel = true;
+    }
+  }
+
+  adjustMobileScreen() {
+    if (this.channel && this.thread) {
+      this.channel = false;
+    } else if (this.channel && this.nav) {
+      this.nav = false;
+    } else if (!this.channel && !this.thread) {
+      this.nav = true;
+    } else if (this.nav && this.thread) {
+      this.nav = false;
     }
   }
 
@@ -68,14 +97,19 @@ export class ChannelPageNavService {
   openChannel() {
     this.channel = true;
     this.thread = false;
+    if (this.mobile) {
+      this.nav = false
+    }
   }
 
   openThread() {
     if (this.mobile) {
       this.channel = false;
       this.thread = true;
-    } else if (this.mediumScreen || this.bigScreen) {
-      this.channel = true;
+    } else if (this.mediumScreen) {
+      this.thread = true;
+      this.nav = false;
+    } else if (this.bigScreen) {
       this.thread = true;
     }
   }
