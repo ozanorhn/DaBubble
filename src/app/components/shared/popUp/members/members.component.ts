fe1dd5c9d8c5250c770ab28c88514c3f8ccb741d
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-05-11 17:08:47 +0200

    marked choiced member

--- src/app/components/shared/popUp/members/members.component.ts
+++ src/app/components/shared/popUp/members/members.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { UserComponent } from '../../user/user.component';
 import { UsersService } from '../../../../services/users/users.service';
 import { OverlayService } from '../../../../pageServices/overlays/overlay.service';
 import { ChannelsService } from '../../../../services/channels/channels.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-members',
@@ -12,32 +13,53 @@ import { ChannelsService } from '../../../../services/channels/channels.service'
   templateUrl: './members.component.html',
   styleUrl: './members.component.scss'
 })
-export class MembersComponent implements OnInit{
+export class MembersComponent implements OnInit {
 
-  members: string[] = [];
+
+  members = signal<string[]>([]);
+
+  filteredUsers = computed(() =>
+    this.usersService.users.filter(user =>
+      this.members().includes(user.id)
+    )
+  );
 
   constructor(
     public usersService: UsersService,
     public overlayService: OverlayService,
     public channelService: ChannelsService
   ) {
+    // this.members.set(this.channelService.channels[this.channelService.currentIndex()].members)
 
-    console.log('Index from channels', this.channelService.currentIndex());
-    console.log('Array from current Channel', this.channelService.channels[this.channelService.currentIndex()].members);
-    this.members = this.channelService.channels[this.channelService.currentIndex()].members
+    // effect(() => {
+    //   console.log('Members Array:', this.members());
+    // });
 
-    console.log(this.filterUsers());
+    this.members.set(this.getCurrentMembers());
+
+    // Reagiere auf Änderungen in Channels oder Index
+    effect(() => {
+      this.members.set(this.getCurrentMembers());
+    });
+  }
+
+  private getCurrentMembers(): string[] {
+    return this.channelService.channels[this.channelService.currentIndex()].members || [];
   }
 
   ngOnInit(): void {
-    this.filterUsers()
+
+    setInterval(() => {
+      console.log('filterd Members Array', this.filteredUsers());
+
+    }, 1000)
+
+
   }
 
-  filterUsers() {
-    return this.usersService.users.filter(user => 
-      this.members.includes(user.id)
-    );
-  }
+
+
+
 
 
 }
