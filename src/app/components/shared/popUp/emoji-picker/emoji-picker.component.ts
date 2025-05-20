b262a61a43a0f37ea088508c34350ce3d477c57f
Nicolas Voges <nicolas.voges@hotmail.com>
2025-05-20 19:58:54 +0200

    Add emoji picker component

---
+++ src/app/components/shared/popUp/emoji-picker/emoji-picker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emoji-picker',
+  imports: [],
+  templateUrl: './emoji-picker.component.html',
+  styleUrl: './emoji-picker.component.scss'
+})
+export class EmojiPickerComponent {
+
+}
