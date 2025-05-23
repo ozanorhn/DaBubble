871da2917ccfe4685dba7ad37fd303e993f431d0
Nicolas Voges <nicolas.voges@hotmail.com>
2025-05-23 13:51:56 +0200

    Make emoji reactions addable for channel messages

--- src/app/components/shared/chat-messages/chat-messages.component.ts
+++ src/app/components/shared/chat-messages/chat-messages.component.ts
@@ -43,15 +43,9 @@ export class ChatMessagesComponent {
   editIndex: number | null = null;
   emojiIndex: number | null = null;
   showEmojiPicker = false;
+  currentEmoji = '';
 
-  ngOnInit(): void {
-    this.currentUser = this.localStorageService.loadObject<User>('currentUser');
-  
-    if (this.currentUser) {
-      this.userService.currentUser = this.currentUser; 
-    }
-  }
-  
+
   constructor(
     public mainNavService: MainNavService,
     public authService: AuthService,
@@ -64,15 +58,64 @@ export class ChatMessagesComponent {
     this.currentUser = this.localStorageService.loadObject<User>('currentUser');
   }
 
-  toggleEmojiPicker() {
-    this.showEmojiPicker = !this.showEmojiPicker;
-    console.log('tooglePicker');
-    
+
+  ngOnInit(): void {
+    this.currentUser = this.localStorageService.loadObject<User>('currentUser');
+    if (this.currentUser) {
+      this.userService.currentUser = this.currentUser;
+    }
   }
 
-  addEmoji(emoji: string) {
-    console.log(emoji);
+
+  toggleEmojiPicker() {
+    this.showEmojiPicker = !this.showEmojiPicker;
   }
+
+
+  addEmoji(emoji: string, message: Message, i: number) {
+    this.currentEmoji = emoji;
+    switch (this.chatType) {
+      case 'channel':
+        const currentUserId = this.userService.currentUser.id;
+        const reactions = this.messageService.messages()[i].reactions;
+        const reaction = reactions.find(r => r.emoji === emoji);
+        if (!reaction) {
+          this.messageService.messages()[i].reactions.push({
+            emoji,
+            users: [currentUserId]
+          });
+        } else {
+          const userIndex = reaction.users.indexOf(currentUserId);
+          let reactionsIndex = this.messageService.messages()[i].reactions.indexOf(reaction);
+          if (userIndex === -1) {
+            this.messageService.messages()[i].reactions[reactionsIndex].users.push(currentUserId);
+            console.log(this.messageService.messages()[i].reactions);
+          } else {
+            this.messageService.messages()[i].reactions[reactionsIndex].users.splice(userIndex, 1);
+            if (reaction.users.length === 0) {
+              const index = reactions.indexOf(reaction);
+              this.messageService.messages()[i].reactions.splice(reactionsIndex, 1);
+            }
+          }
+          this.messageService.editMessage(message);
+        }
+
+        break;
+      case 'dm':
+
+        break;
+      case 'thread':
+
+        break;
+
+      default:
+        break;
+    }
+    console.log(emoji);
+    console.log(message);
+
+  }
+
 
   toggleEditInput(index: number): void {
     if (this.editIndex === index) {
