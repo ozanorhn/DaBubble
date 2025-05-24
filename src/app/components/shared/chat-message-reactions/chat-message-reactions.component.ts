8729c7674e68a444771af0bddb0dd0fee5109e80
Nicolas Voges <nicolas.voges@hotmail.com>
2025-05-25 00:55:01 +0200

    Feat: Toggle reaction by clicking a reaction

--- src/app/components/shared/chat-message-reactions/chat-message-reactions.component.ts
+++ src/app/components/shared/chat-message-reactions/chat-message-reactions.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Message } from '../../../classes/message.class';
 import { DM } from '../../../interfaces/dm';
 import { UsersService } from '../../../services/users/users.service';
@@ -11,6 +11,7 @@ import { UsersService } from '../../../services/users/users.service';
 })
 export class ChatMessageReactionsComponent {
   @Input() message: Message | DM = new Message();
+  @Output() emojiPicked = new EventEmitter<string>();
 
 
   constructor(
@@ -25,5 +26,10 @@ export class ChatMessageReactionsComponent {
     } else {
       return user?.name;
     }
+  }
+
+
+  react(emoji: string) {
+    this.emojiPicked.emit(emoji);
   }
 }
