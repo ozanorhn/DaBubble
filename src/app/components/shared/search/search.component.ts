536099f5ef20daf350f12facd18702a74ef877c2
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-05-29 13:50:50 +0200

    fix: search dropdown

--- src/app/components/shared/search/search.component.ts
+++ src/app/components/shared/search/search.component.ts
@@ -2,12 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FilterService } from '../../../pageServices/filters/filter.service';
+import { Channel } from '../../../classes/channel.class';
+import { User } from '../../../classes/user.class';
+import { MainNavService } from '../../../pageServices/navigates/main-nav.service';
+import { UserComponent } from '../user/user.component';
 
 @Component({
   selector: 'app-search',
   imports: [
     FormsModule,
-    CommonModule
+    CommonModule,
+    UserComponent
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
@@ -16,7 +21,22 @@ export class SearchComponent {
 
   devspace = ''
 
-  constructor(public filterService: FilterService) {}
+  constructor(
+    public filterService: FilterService,
+    public mainNavService: MainNavService
+  ) { }
+
+  user = new User();
+  channel = new Channel();
+
+
+  isUser(item: any): item is User {
+    return 'name' in item && 'avatar' in item; // Anpassen an Ihre User-Properties
+  }
+
+  isChannel(item: any): item is Channel {
+    return 'name' in item && 'id' in item; // Anpassen an Ihre Channel-Properties
+  }
 
 
 }
