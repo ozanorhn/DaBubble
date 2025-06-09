6a4f12f91cfaf52d7939a086c81fc971a2d50159
Jean Pondy <manbayesse@gmail.com>
2025-06-09 03:17:28 +0200

    konto erstellen/ required

--- src/app/components/shared/popUp/profile/profile.component.ts
+++ src/app/components/shared/popUp/profile/profile.component.ts
@@ -23,7 +23,7 @@ export class ProfileComponent {
   editProfil = false;
   usersProfil = false;
   changeName = '';
-  avatars = [1, 2, 3, 4, 5, 6, 7, 8];
+  avatars = [0,1, 2, 3, 4, 5, 6, 7, 8];
 
 
   constructor(
