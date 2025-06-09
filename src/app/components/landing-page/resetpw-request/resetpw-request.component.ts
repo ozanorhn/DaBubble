6c4bbdcd7ed4cf1287d28c34ed997f4ff8262806
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-06-09 16:40:25 +0200

    refactor: resetpw overlay

--- src/app/components/landing-page/resetpw-request/resetpw-request.component.ts
+++ src/app/components/landing-page/resetpw-request/resetpw-request.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { LandingPageService } from '../../../pageServices/navigates/landing-nav.service';
 import { FormsModule } from '@angular/forms';
- import { CommonModule } from '@angular/common';
- import { Auth, sendPasswordResetEmail } from '@angular/fire/auth'; 
+import { CommonModule } from '@angular/common';
+import { Auth, sendPasswordResetEmail } from '@angular/fire/auth';
 import { UsersService } from '../../../services/users/users.service';
 import { Router, RouterModule } from '@angular/router';
 
@@ -14,14 +14,21 @@ import { Router, RouterModule } from '@angular/router';
   styleUrl: './resetpw-request.component.scss'
 })
 export class ResetpwRequestComponent {
+  
   email: string = '';
   showMessage: boolean = false;
-  
-  constructor(public landing: LandingPageService, private auth: Auth, private router: Router ) {}
 
-  goToLogin(){
+  constructor(
+    public landing: LandingPageService,
+    private auth: Auth,
+    private router: Router
+  ) { }
+
+
+  goToLogin() {
     this.landing.landing.set('login')
   }
+
 
   async sendeResetAnfrage() {
     if (!this.email) {
@@ -29,24 +36,24 @@ export class ResetpwRequestComponent {
       console.log('Bitte geben Sie eine gültige E-Mail-Adresse ein.');
       return;
     }
-
     try {
       await sendPasswordResetEmail(this.auth, this.email);
-      this. openMessage();
-     
+      this.openMessage();
+
     } catch (err) {
       console.error(err);
       alert('Fehler beim Senden der E-Mail. Bitte überprüfen Sie die Adresse.');
     }
   }
 
+
   openMessage() {
     this.showMessage = true;
     setTimeout(() => {
       this.showMessage = false;
-      this.goToLogin(); 
-    }, 3000); 
+      this.goToLogin();
+    }, 3000);
   }
-  
-  
+
+
 }
